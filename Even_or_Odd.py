32eea6e7aff55f8359763e37f1e52bd20b025c37
Chetan S. Chavan <chetan.chvn2@gmail.com>
2025-07-14 12:51:15 +0530

    Add files via upload

---
+++ Even_or_Odd.py
@@ -0,0 +1,9 @@
+#Take the input from use
+num = int(input("Enter the number: "))
+
+#check whether the num is divisible by 2 or not if divisble then even else odd
+if num % 2 == 0 :
+    print(num ,"is an even number")
+else:
+    print(num ,"is an odd number")
+
---
+++ sum_of_number.py
@@ -0,0 +1,7 @@
+total_sum = 0  # Initialize a variable to store the sum
+
+# Iterate through numbers from 1 to 50 (range(1, 51) includes 1 but excludes 51)
+for n in range(1, 51):
+    total_sum += n  # Add each number to the total_sum
+
+print("The sum of numbers from 1 to 50 is:", total_sum)
